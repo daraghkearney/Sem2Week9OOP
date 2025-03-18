1062f4b892db9aecc39f29d6a59d57e21d6758db
DARAGHKEARNEY-STUDEN <g00438215@atu.ie>
2025-03-18 11:35:55 +0000

    Added Multiplication Feature

--- src/main/java/ie/atu/week9sem2/Calculator.java
+++ src/main/java/ie/atu/week9sem2/Calculator.java
@@ -29,4 +29,12 @@ public class Calculator {
         }
         return (int) result;
     }
+
+    public int multiply(int firstNum, int secondNum) {
+        long result = (long) firstNum * secondNum;
+        if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
+            throw new UnsupportedOperationException("Result exceeds valid integer range");
+        }
+        return (int) result;
+    }
 }

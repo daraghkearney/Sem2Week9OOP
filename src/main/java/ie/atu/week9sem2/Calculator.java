1595b9efa0a4e3ee5667fc23cd178db2c758815c
DARAGHKEARNEY-STUDEN <g00438215@atu.ie>
2025-03-18 10:49:41 +0000

    Added Divide Feature

--- src/main/java/ie/atu/week9sem2/Calculator.java
+++ src/main/java/ie/atu/week9sem2/Calculator.java
@@ -2,12 +2,21 @@ package ie.atu.week9sem2;
 
 public class Calculator {
 
-    public int add(int firstNum, secondNum)
+    public int add(int firstNum, int secondNum)
     {
         long result = (long) firstNum + secondNum;
         if(result > Integer.MAX_VALUE)
         {
             throw new IllegalArgumentException("This value is too big");
+        }
+        return (int) result;
+    }
+
+    public int divide(int firstNum, int secondNum) {
+        long result = (long) firstNum / secondNum;
+        if(result > Integer.MAX_VALUE)
+        {
+            throw new ArithmeticException("This value is too big");
         }
         return (int) result;
     }

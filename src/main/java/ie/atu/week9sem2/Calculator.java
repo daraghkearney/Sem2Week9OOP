bd9d74a9092d29b60698c09a9446954b330b6d6b
DARAGHKEARNEY-STUDEN <g00438215@atu.ie>
2025-03-18 09:47:42 +0000

    Add Feature

---
+++ src/main/java/ie/atu/week9sem2/Calculator.java
@@ -0,0 +1,14 @@
+package ie.atu.week9sem2;
+
+public class Calculator {
+
+    public int add(int firstNum, secondNum)
+    {
+        long result = (long) firstNum + secondNum;
+        if(result > Integer.MAX_VALUE)
+        {
+            throw new IllegalArgumentException("This value is too big");
+        }
+        return (int) result;
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,27 @@
+import ie.atu.week9sem2.Calculator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CalculatorTest {
+
+    Calculator myCalc;
+
+    @BeforeEach
+    void setup() {
+        myCalc = new Calculator();
+    }
+
+    @Test
+    void testAddSuccess() {
+        assertEquals(8, myCalc.add(4, 4));
+    }
+
+    @Test
+    void testAddFail() {
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> {myCalc.add(Integer.MAX_VALUE, 4);});
+        assertEquals("This value is too big1", ex.getMessage());
+    }
+}
